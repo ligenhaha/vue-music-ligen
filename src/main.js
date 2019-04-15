364cc14ce00be748096013926746de4a04b3acdd
ligenhaha <47690856+ligenhaha@users.noreply.github.com>
2019-04-15 20:39:15 +0800

    ligen first

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+import store from './store'
+import 'common/stylus/index.styl'
+// body 下面所有div 点击都没有300毫秒的延迟
+fastclick.attach(document.body)
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  store,
+  router
+})
