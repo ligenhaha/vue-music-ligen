3ffefccef0531fc695419b169a820c977cb11a45
ligenhaha <47690856+ligenhaha@users.noreply.github.com>
2019-04-19 07:05:35 +0800

    20190419

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="song-list">
     <ul>
-      <li v-for="(song,index) in songs" class="item" :key="index">
+      <li @click="selectItem(song,index)" v-for="(song,index) in songs" class="item" :key="index">
         <div class="content">
           <h2 class="name">{{song.name}}</h2>
           <p class="desc">{{getDesc(song)}}</p>
@@ -24,6 +24,9 @@
     methods: {
       getDesc (song) {
         return `${song.singer}.${song.album}`
+      },
+      selectItem (item, index) {
+        this.$emit('select', item, index)
       }
     }
   }
@@ -39,13 +42,16 @@
       box-sizing: border-box
       height: 64px
       font-size: $font-size-medium
+
       .content
         flex: 1
         line-height: 20px
         overflow: hidden
+
         .name
           no-wrap()
           color: $color-text
+
         .desc
           no-wrap()
           margin-top: 4px
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -17,7 +17,7 @@
     <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list"
             ref="list">
       <div class="song-list-wrapper">
-        <song-list :songs="songs"></song-list>
+        <song-list @select="selectItem" :songs="songs"></song-list>
       </div>
       <div class="loading-container" v-show="!songs.length">
         <loading></loading>
@@ -31,6 +31,7 @@
   import SongList from 'base/song-list/song-list'
   import {prefixStyle} from 'common/js/dom'
   import Loading from 'base/loading/loading'
+  import {mapActions} from 'vuex'
 
   const RESERVED_HEIGHT = 40
   const transtorm = prefixStyle('transform')
@@ -116,7 +117,16 @@
       },
       back () {
         this.$router.back()
-      }
+      },
+      selectItem (item, index) {
+        this.selectPlay({
+          list: this.songs,
+          index
+        })
+      },
+      ...mapActions([
+        'selectPlay'
+      ])
     }
   }
 </script>
