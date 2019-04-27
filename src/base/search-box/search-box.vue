d22f9bed138fb1d96d6a429076db6cc00f62a6e8
ligenhaha <47690856+ligenhaha@users.noreply.github.com>
2019-04-28 07:10:00 +0800

    20190428

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: 'search-box'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/top-list/top-list.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: 'top-list'
+  }
+</script>
+
+<style scoped>
+
+</style>
